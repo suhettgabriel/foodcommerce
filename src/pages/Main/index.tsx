0bcdc507e15b965d6db0bafcf4a1f96a0b151242
suhettgabriel <suhettgabriel1983@gmail.com>
2023-05-14 08:25:10 -0300

    primeiro commit

---
+++ src/pages/Main/index.tsx
@@ -0,0 +1,11 @@
+import { Sidebar } from '../../components/Sidebar'
+import { Container } from './styles'
+
+export default function Main() {
+  return (
+    <Container>
+      <Sidebar />
+      <h1>Main</h1>
+    </Container>
+  )
+}
